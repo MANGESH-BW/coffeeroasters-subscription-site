0dfb270b11ec8091f3ad067a2b875ee4d288aeb1
MANGESH-BW <mangesh.bhargande@siliconstack.com.au>
2021-12-14 14:51:33 +0530

    Home screen is completed of mobile version.

--- main.js
+++ main.js
@@ -1,27 +1,43 @@
-const btnHamburger = document.querySelector('#btnHamburger');
+// const btnHamburger = document.querySelector('#btnHamburger');
+// const header = document.querySelector('.header');
+// const overlay = document.querySelector('.overlay');
+// const header__menu = document.querySelector('.header__menu');
+// const fadeElems = document.querySelectorAll('.has-fade');
+// btnHamburger.addEventListener('click', function(){
+//     console.log('click hamburger');
+
+//     if(header.classList.contains('open')){ // Close Hamburger Menu
+//     header.classList.remove('open');
+//     fadeElems.forEach(function(element){
+//         overlay.classList.remove('fade-in');
+//         overlay.classList.add('fade-out');
+//         header__menu.classList.remove('fade-in');
+//         header__menu.classList.add('fade-out');
+//     });
+//     }
+//     else { // Open Hamburger Menu
+//         header.classList.add('open');
+//         fadeElems.forEach(function(element){
+//             overlay.classList.remove('fade-out');
+//             overlay.classList.add('fade-in');
+//             header__menu.classList.remove('fade-out');
+//             header__menu.classList.add('fade-in');
+//         });
+//     }
+// });
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
-const header__menu = document.querySelector('.header__menu');
-const fadeElems = document.querySelectorAll('.has-fade');
-btnHamburger.addEventListener('click', function(){
-    console.log('click hamburger');
+const hamburger = document.querySelector(".ham");  
+const headermenu = document.querySelector(".menu");  
 
-    if(header.classList.contains('open')){ // Close Hamburger Menu
-    header.classList.remove('open');
-    fadeElems.forEach(function(element){
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
-        header__menu.classList.remove('fade-in');
-        header__menu.classList.add('fade-out');
-    });
-    }
-    else { // Open Hamburger Menu
-        header.classList.add('open');
-        fadeElems.forEach(function(element){
-            overlay.classList.remove('fade-out');
-            overlay.classList.add('fade-in');
-            header__menu.classList.remove('fade-out');
-            header__menu.classList.add('fade-in');
-        });
-    }
-});
+
+
+hamburger.addEventListener('click', () => {  
+   
+ hamburger.classList.toggle("change")  
+ headermenu.classList.toggle("nav-change")  
+ overlay.classList.toggle("overlay")
+});  
+
+
+

e3b5990c0fb28e4b37201e9a32755ba9822d1da7
MANGESH-BW <mangesh.bhargande@siliconstack.com.au>
2022-01-04 12:46:17 +0530

    Home screen and About us screen is completed.

--- about/about.js
+++ about/about.js
@@ -1,1 +1,14 @@
 
+// const header = document.querySelector('.header');
+// const overlay = document.querySelector('.overlay');
+// const hamburger = document.querySelector(".ham");  
+// const headermenu = document.querySelector(".overlay_menu");  
+
+
+
+// hamburger.addEventListener('click', () => {  
+   
+//  hamburger.classList.toggle("change")  
+//  headermenu.classList.toggle("nav-change")  
+//  overlay.classList.toggle("overlay")
+// });  
